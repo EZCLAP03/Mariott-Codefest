4b66346404dd4b66e892aff51f893c1c563b5165
Episode 33 <mowilliam416@gmail.com>
2024-10-19 16:11:07 -0400

    Update api.py

--- api.py
+++ api.py
@@ -3,6 +3,9 @@ import requests
 def get_score(state):
     url = f"https://api.w1111am.xyz:8443/get_score?state={state}"
     response = requests.get(url)
+    data = response.json()
+    if 'error' in data:
+        return "State not found"
     # example response {'Environmental Score': 0.8799876675480947, 'State': 'West Virginia'}
     # return ENV score
-    return response.json()['Environmental Score']
+    return data['Environmental Score']
