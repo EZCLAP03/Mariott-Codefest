5acec425c513ad5788f22f4ddb89f5ef6a41e530
Harisai <64903659+EZCLAP03@users.noreply.github.com>
2024-10-19 00:56:23 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+from kivy.lang import Builder
+from kivymd.uix.label import MDLabel
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.textfield import MDTextField
+from kivy.config import Config
+import time; 
+import requests; 
+import json; 
+
+params = {
+
+    'grant_type': 'client_credentials&client_id={client_id}&client_secret={client_secret}',
+}
+response = requests.get("https://test.api.amadeus.com/v1/security/oauth2/token") 
+
+KV = '''
+ScreenManager:
+    MainScreen:
+
+<MainScreen>:
+    name: 'main'
+    MDTextField:
+        id: text_field
+        hint_text: "Where do you want to go?"
+        pos_hint: {"center_x": 0.51, "center_y": 0.7}
+        size_hint_x: None
+        width: 300
+    MDTextField:
+        id: text_field
+        hint_text: "What is your flight number?"
+        pos_hint: {"center_x": 0.51, "center_y": 0.6}
+        size_hint_x: None
+        width: 300
+    MDRaisedButton:
+        text: "Submit"
+        pos_hint: {"center_x": 0.5, "center_y": 0.5}
+        on_release: app.on_button_press()
+'''
+class Header(MDLabel):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.text = "OnePass"
+        self.halign = "center"
+        self.pos_hint = {"center_x": 0.5, "center_y": 0.9}
+        self.font_style = "H6"
+
+
+class MainScreen(Screen):
+    pass
+
+class MainApp(MDApp):
+    def build(self):
+        self.start_time = time.time()
+        self.title = "My KivyMD App"
+        self.window.size = (800, 600)
+        return Builder.load_string(KV)
+
+    def on_button_press(self):
+        text = self.root.get_screen('main').ids.text_field.text
+        print(f'Text entered: {text}')
+
+    def on_stop(self):
+        end_time = time.time()
+        print(f'Time taken: {(int) (end_time - self.start_time)}')
+        
+
+if __name__ == '__main__':
+    MainApp().run()
